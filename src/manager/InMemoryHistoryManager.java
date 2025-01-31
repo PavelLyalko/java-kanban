f97c715bbc8fc5c03727a1a16f6274fc0486e32a
Pavel Lyalko <pasha.lyalko@yandex.ru>
2025-01-31 13:09:08 +0300

    убрал импорт

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -5,10 +5,10 @@ import utils.Node;
 
 import java.util.*;
 
-public class InMemoryHistoryManager implements HistoryManager{
+public class InMemoryHistoryManager implements HistoryManager {
     private HistoryLinkedHashMap<Integer, Task> historyList = new HistoryLinkedHashMap<>();
 
-    private Map<Integer, Node> historyMap= new HashMap<>();
+    private Map<Integer, Node> historyMap = new HashMap<>();
 
     public Map<Integer, Node> getHistoryMap() {
         return historyMap;
@@ -29,12 +29,12 @@ public class InMemoryHistoryManager implements HistoryManager{
         return historyList.getTasks();
     }
 
-    class HistoryLinkedHashMap<K,V> extends LinkedHashMap<K,V> {
+    class HistoryLinkedHashMap<K, V> extends LinkedHashMap<K, V> {
         public void linkLast(Task task) {
             if (historyMap.containsKey(task.getId())) {
                 remove(historyMap.get(task.getId()).getIndex());
                 historyList.put(task.getId(), task);
-                historyMap.put(task.getId(), new Node<>(task.getId(),null, task, null));
+                historyMap.put(task.getId(), new Node<>(task.getId(), null, task, null));
             } else {
                 historyList.put(task.getId(), task);
                 historyMap.put(task.getId(), new Node<>(task.getId(), null, task, null));
