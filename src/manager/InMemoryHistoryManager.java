242efb96bf1183f640abf6338e30b2909cef9e54
Pavel Lyalko <pasha.lyalko@yandex.ru>
2025-02-07 14:26:13 +0300

    правки ревью

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -3,15 +3,11 @@ package manager;
 import tasks.Task;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager {
     private HistoryLinkedHashMap<Integer, Task> historyList = new HistoryLinkedHashMap<>();
-
-    private Map<Integer, Task> historyMap = new HashMap<>();
 
     @Override
     public void add(Task task) {
@@ -20,7 +16,7 @@ public class InMemoryHistoryManager implements HistoryManager {
 
     @Override
     public void remove(int id) {
-        historyList.remove(historyMap.get(id).getId());
+        historyList.remove(id);
     }
 
     @Override
@@ -30,11 +26,10 @@ public class InMemoryHistoryManager implements HistoryManager {
 
     class HistoryLinkedHashMap<K, V> extends LinkedHashMap<K, V> {
         public void linkLast(Task task) {
-            if (historyMap.containsKey(task.getId())) {
+            if (containsKey(task.getId())) {
                 historyList.remove(task.getId());
             }
             historyList.put(task.getId(), task);
-            historyMap.put(task.getId(),  task);
         }
 
         public List<Task> getTasks() {
